58bf436d618b15a16977c9a2aeb5630f4061f81a
Eber Guerra <eber.g@MacBook-Air-de-Eber.local>
2020-03-21 23:07:52 -0600

    Primer

---
+++ Laboratorio4/Controllers/FileUploadController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Laboratorio4.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Laboratorio4.Controllers
+{
+    [Route("api/[controller]")]
+    public class FileUploadController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+        Archivo archivo = new Archivo();
+
+        public FileUploadController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public class FileUploadAPI
+        {
+            public IFormFile files { get; set; }
+        }
+
+        public async Task<string> Post(FileUploadAPI objFile)
+        {
+
+            string[] lines = { "hola" };
+            try
+            {
+                if (objFile.files.Length > 0)
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "Upload"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "Upload");
+                    }
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "Upload" + objFile.files.FileName))
+                    {
+                        objFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+
+
+
+                        
+                        return "\\Upload\\" + objFile.files.FileName;
+
+                    }
+
+
+
+                }
+                else
+                {
+                    return "Failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message.ToString();
+            }
+        }
+
+    }
+}
---
+++ Laboratorio4/Models/Archivo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Laboratorio4.Models
+{
+    public class Archivo
+    {
+        private Dictionary<char, int> TablaCaracteres { get; set; }
+        private List<string> ListaTexto { get; set; } 
+        public const int TamañoBuffer = 1040;
+        
+
+        public Archivo()
+        {
+            TablaCaracteres = new Dictionary<char, int>();
+            ListaTexto = new List<string>();
+        }
+
+
+        public void LeerArchivoTexto(FileStream fileStream)
+        {
+            char[] Buffer = new char[TamañoBuffer];
+            var LineaLeida = "";
+            var i = 0;
+            using(var sr = new StreamReader("//Users//eber.g//Downloads//PRUEBAS//Prue.txt"))
+            {
+                //Mandar bloques de texto
+                LineaLeida = sr.ReadLine();
+                while( LineaLeida != null)
+                {
+                    LineaLeida=(sr.ReadBlock(Buffer, i, TamañoBuffer)).ToString();
+                    i += TamañoBuffer;
+                    LineaLeida = sr.ReadLine();
+                }
+               
+            }
+        }
+        public void ObtenerCaracteresBase(string Characteres)
+        {
+
+        }
+    }
+}
